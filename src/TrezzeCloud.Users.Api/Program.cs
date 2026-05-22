2ad278153450a376b44537f9cd32dd6971189929
Guilherme Monteiro Massi <GMMASSI@der.sp>
2026-05-22 16:02:46 -0300

    Swagger

--- src/TrezzeCloud.Users.Api/Program.cs
+++ src/TrezzeCloud.Users.Api/Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using Microsoft.OpenApi.Models;
 using TrezzeCloud.Users.Api.Configurations;
 using TrezzeCloud.Users.Identity;
 
@@ -7,7 +8,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Informe somente o token JWT."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddUsersIdentity(builder.Configuration);
 builder.Services.AddJwtConfiguration(builder.Configuration);
